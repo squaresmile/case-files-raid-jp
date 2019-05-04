7bb2a6a26d3d853a8aa234e72212e3e8ed145095
squaresmile <squaresmile@protonmail.com>
2019-05-03 20:25:27 -0400

    Put make chart in rate_screenshot_parse

--- raid_screenshot_parse.py
+++ raid_screenshot_parse.py
@@ -5,9 +5,12 @@ from datetime import datetime
 from datetime import timedelta
 import numpy as np
 import pandas as pd
+import pandas.plotting._converter as pandacnv
+import matplotlib.pyplot as plt
 import pytesseract
 import cv2
 
+HP = 60000000
 
 def get_qp_from_text(text):
     qp = 0
@@ -18,6 +21,9 @@ def get_qp_from_text(text):
         power *= 1000
 
     return qp
+
+if os.path.exists("manual_data.csv"):
+    manual_data = pd.read_csv("manual_data.csv", parse_dates=["Time"])
 
 dict_df = {"File": [], "Time": [], "Kills":[]}
 with open("timezone.json", "r", encoding='utf-8') as f:
@@ -57,8 +63,42 @@ for user in user_list:
             dict_df["Kills"].append(ocr)
             dict_df["File"].append(file)
         if ocr == 0:
-            print(f"{file}, {time}")
+            if manual_data is not None and file not in list(manual_data["File"]):
+                print(f"{file}, {time}")
 
 df = pd.DataFrame.from_dict(dict_df)
 df = df.sort_values("Time")
 df.to_csv("parsed_data.csv", index=False)
+
+if os.path.exists("manual_data.csv"):
+    manual = pd.read_csv("manual_data.csv", parse_dates=["Time"])
+    raid_data = pd.concat([manual, df])
+else:
+    raid_data = df
+raid_data = raid_data.drop_duplicates("Time").sort_values("Time")
+
+# Increasing kill count only
+raid_data = raid_data[raid_data["Kills"] > raid_data["Kills"].shift(1).fillna(0)]
+
+x = raid_data["Time"][1:]
+y = raid_data["Kills"].diff()[1:] / raid_data['Time'].diff().dt.total_seconds()[1:]
+x = x[1:-1]
+y = y.rolling(3, center=True).mean()[1:-1]
+
+pandacnv.register()
+plt.style.use('seaborn')
+fig, ax = plt.subplots(figsize=(14, 7.5))
+ax.plot(x, y)
+# plt.plot_date(x, y)
+# fig.autofmt_xdate()
+update_time = x.iloc[-1]
+plt.title(f"JP case files raid KPS - updated {update_time:%Y-%m-%d %H:%M} JST")
+plt.xlabel("Japan Standard Time")
+plt.ylabel("Kills per Second")
+plt.savefig("chart.png", dpi=200, bbox_inches='tight')
+
+avg_rate = y[-10:].mean()
+time_to_kill = (HP - raid_data["Kills"].iloc[-1])/avg_rate
+time_to_kill = pd.to_timedelta(time_to_kill, unit='s')
+
+print(f"Time to death: {time_to_kill}")
